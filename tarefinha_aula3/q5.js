e118fa5bdde4592c6721422688b00f7d2347c967
Camila <camila.th.s.medeiros@gmail.com>
2026-04-16 09:33:14 -0300

    Implement data collection in a 3x2 array format

--- tarefinha_aula3/q5.js
+++ tarefinha_aula3/q5.js
@@ -7,3 +7,18 @@
 // idade 10
 // ja_foi_a_Disney false
 
+const prompt = require('prompt-sync')(); 
+let nome = prompt('Escreva seu nome, sinhô(ou sinhora): ') 
+let idade = Number(prompt('Agora ponha sua idade: ')) 
+let disney = prompt('Só pra eu saber mesmo cara, tu já foi pra a Disney (true/false)?? ')
+disney = (disney.toLowerCase() === 'true'); 
+
+let matriz_dados_pessoa = [ //aqui está a matriz com 3 linhas e 2 colunas
+    ['nome ', nome],
+    ['idade ', idade],
+    ['ja_foi_a_Disney ',disney]
+]
+
+for (let linha of matriz_dados_pessoa){ //o for in não funciona, pq ele serve para mostrar posições, e o for of para mostrar valores
+    console.log(linha[0],linha[1]) //linha 0 é a primeira coluna e linha 1 é a segunda da linha da matriz
+}
